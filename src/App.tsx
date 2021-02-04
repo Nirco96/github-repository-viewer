85ef03d6c0a6a90db32da2119b4114ae4589655a
Nir <99nirc99@gmail.com>
2021-02-04 22:20:57 +0200

    removed personal access token

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,16 @@ import {useInfiniteScroll} from "./UseInfiniteScroll";
 import axios, {AxiosError, AxiosResponse} from "axios";
 import loadingSvg from "./Rolling-1s-100px.svg"
 
-const ACCESS_TOKEN = `1be428951bffe33129ae461117525afd9f591634`;
+// const ACCESS_TOKEN = `1be428951bffe33129ae461117525afd9f591634`;
 const COMMITS_PER_PAGE = 10;
 export const BASE_URL = `https://api.github.com/repos`;
 
 export const client = axios.create({
   baseURL: BASE_URL,
-  headers: {
-    'Authorization': `token ${ACCESS_TOKEN}`
-  }
+  // Used for development
+  // headers: {
+    // 'Authorization': `token ${ACCESS_TOKEN}`
+  // }
 })
 
 function fetchCommits(repoQuery: RepositoryFormResult, page: number) {
@@ -85,7 +86,7 @@ function App() {
         </div>
       </section>
       <section className={`section`}>
-        {error ? error : <CommitsList commits={commits}></CommitsList>}
+        {error ? <p className={`loading`}>error</p> : <CommitsList commits={commits}></CommitsList>}
         <div className={`loading`} ref={scrollEl}>{isLoading ? <img alt={'loading...'} src={loadingSvg}/> : ''}</div>
       </section>
       {/*<div className={`heatmapContainer`}>*/}
